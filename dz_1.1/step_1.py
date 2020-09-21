94a3ee344d287d51aee347efd0904ba9568ce7d6
Alexa-PL <platanaya48@gmail.com>
2020-09-22 02:54:19 +0300

    1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.

---
+++ dz_1.1/step_1.py
@@ -0,0 +1,29 @@
+
+a = 2
+b = 4
+c = 3
+print("a=2")
+print("b=4")
+print("c=3")
+number1 = int(input("Введите число d"))
+number2 = int(input("Введите  число e"))
+string1 = str(input("Введите строку f"))
+string2 = str(input("Введите  строку g"))
+print(number1)
+print(number2)
+print(string1)
+print(string2)
+summ = print(int(number1)+int(number2))
+text = print(str(string1)+str(string2))
+
+
+
+
+
+
+
+
+
+
+
+
